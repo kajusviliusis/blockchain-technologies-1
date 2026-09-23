2121d69e5adf2f5461c0d08b428f0e97fe294157
kajusviliusis <kajus.viliusis0@gmail.com>
2026-09-23 21:57:21 +0300

    feat: implement input byte mixing

--- src/main/java/hashing/HashFunctionV01.java
+++ src/main/java/hashing/HashFunctionV01.java
@@ -11,6 +11,19 @@ public final class HashFunctionV01 implements HashFunction {
     public String hash(byte[] input) {
         int[] state = INITIAL_STATE.clone();
 
+        for (int i = 0; i < input.length; i++) {
+            int unsignedByte = input[i] & 0xFF;
+            int currentIndex = i % state.length;
+            int nextIndex = (currentIndex + 1) % state.length;
+
+            state[currentIndex] ^= unsignedByte + i;
+
+            int rotation = (unsignedByte % 13) + 5;
+            state[currentIndex] =
+                    Integer.rotateLeft(state[currentIndex], rotation);
+
+            state[currentIndex] += state[nextIndex];
+        }
+
         throw new UnsupportedOperationException("not implemented yet");
     }
-}
